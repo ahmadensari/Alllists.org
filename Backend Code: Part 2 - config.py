9b753a7327d42dafa7a8d990055b8fe40fe5c49c
ahmadensari <ahmadensari@gmail.com>
2024-11-24 13:19:55 +0500

    Create Backend Code: Part 2 - config.py

---
+++ Backend Code: Part 2 - config.py
@@ -0,0 +1,7 @@
+python
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@localhost/alllists')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
