6d158035369ff040b5996832ccd2015f8cc26ca7
ahmadensari <ahmadensari@gmail.com>
2024-11-24 18:03:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from routes.auth import auth_routes
 from routes.list import list_routes
 from routes.payment import payment_routes
-from database import db  # Import db from the shared database module
+from database import db
 from config import Config
 
 # Initialize Flask app
@@ -20,7 +20,8 @@ app.register_blueprint(payment_routes, url_prefix='/payments')
 # Ensure database tables are created before the first request
 @app.before_first_request
 def create_tables():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
