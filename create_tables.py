59988317b143cd0fe90232d1d0fe1c222aa50d91
ahmadensari <ahmadensari@gmail.com>
2024-11-24 18:39:48 +0500

    Delete create_tables.py

--- create_tables.py
+++
@@ -1,7 +0,0 @@
-from main import app
-from database import db
-from models import User, List, Transaction  # Ensure all models are imported here
-
-with app.app_context():
-    db.create_all()
-    print("Database tables created.")
