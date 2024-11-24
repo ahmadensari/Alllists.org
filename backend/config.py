13d41e249915ff888398d779fcb2a9abd9cf6d05
ahmadensari <ahmadensari@gmail.com>
2024-11-24 20:54:16 +0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@localhost/alllists')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql://ahmadensari:Mahmood#7407Ifi@localhost/alllists_db'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
