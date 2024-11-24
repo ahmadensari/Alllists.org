8c536cd9c424320b5ac19439e7eabf25826dcb01
ahmadensari <ahmadensari@gmail.com>
2024-11-24 18:27:02 +0500

    Create list.py

---
+++ backend/models/list.py
@@ -0,0 +1,7 @@
+rom main import db
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
